1b92f94072a9cfc12dcce5daddcf322b792cc570
Sashank D <harsha.desu@hotmail.com>
2023-09-08 23:44:27 +0530

    wip: userpage

--- ehr-soulbound/src/app/user-page/page.tsx
+++ ehr-soulbound/src/app/user-page/page.tsx
@@ -3,15 +3,11 @@ import Link from "next/link";
 export default function UserPage() {
   return (
     <div className="flex">
-        <div className="p-5 ml-auto">
-            <button className="border-[2px] rounded-full border-[#F6D1CC] py-4 px-4 hover:bg-[#3898EC]/25 font-sans text-base font-medium text-[#0B1EB5]">
-                <Link href="/">Add Doctor's Visit</Link>
-            </button>
+        <div className="pt-10 pr-5 ml-auto">
+            <Link className="border-[2px] rounded-full border-[#F6D1CC] py-4 px-4 hover:bg-[#3898EC]/25 font-sans text-base font-medium text-[#0B1EB5]" href="/doctor-visit">Add Doctor's Visit</Link>
         </div>
-        <div className="p-5 mr-auto">
-            <button className="border-[2px] rounded-full border-[#F6D1CC] py-4 px-4 hover:bg-[#3898EC]/25 font-sans text-base font-medium text-[#0B1EB5]">
-                <Link href="/">Add Lab Reports</Link>
-            </button>
+        <div className="pt-10 pl-5 mr-auto">
+            <Link className="border-[2px] rounded-full border-[#F6D1CC] py-4 px-4 hover:bg-[#3898EC]/25 font-sans text-base font-medium text-[#0B1EB5]" href="/">Add Lab Reports</Link>
         </div>
     </div>
   )
