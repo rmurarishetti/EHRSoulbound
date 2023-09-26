200f76ee1c0e8cd27b3186c9efa2646fd4720251
Sashank D <harsha.desu@hotmail.com>
2023-09-26 13:02:38 +0530

    wip: doctor-visit, lab-report

--- ehr-soulbound/src/app/doctor-visit/page.tsx
+++ ehr-soulbound/src/app/doctor-visit/page.tsx
@@ -5,6 +5,8 @@ import * as Form from '@radix-ui/react-form'
 import * as Select from '@radix-ui/react-select'
 import { CheckIcon, ChevronUpIcon, ChevronDownIcon } from '@radix-ui/react-icons'
 import Image from "next/image";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 const DoctorVisitForm = () => {
     const [state, setState] = useState({
@@ -69,10 +71,10 @@ const DoctorVisitForm = () => {
                 <Form.Field className="grid mb-10" name="userdisease">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="font-quicksand pl-4 text-2xl font-semibold text-[#0B1E5B]">
-                        Disease
+                            Disease
                         </Form.Label>
                         <Form.Message className="font-quicksand text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
-                        Please enter disease name
+                            Please enter disease name
                         </Form.Message>
                     </div>
                     <Form.Control asChild>
@@ -88,10 +90,10 @@ const DoctorVisitForm = () => {
                 <Form.Field className="grid mb-10" name="usersymptoms">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="font-quicksand pl-4 text-2xl font-semibold text-[#0B1E5B]">
-                        Symptoms
+                            Symptoms
                         </Form.Label>
                         <Form.Message className="font-quicksand text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
-                        Please enter symptoms
+                            Please enter symptoms
                         </Form.Message>
                     </div>
                     <Form.Control asChild>
@@ -107,10 +109,10 @@ const DoctorVisitForm = () => {
                 <Form.Field className="grid mb-10" name="usermeds">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="font-quicksand pl-4 text-2xl font-semibold text-[#0B1E5B]">
-                        Medicines taken
+                            Medicines taken
                         </Form.Label>
                         <Form.Message className="font-quicksand text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
-                        Please enter medicines
+                            Please enter medicines
                         </Form.Message>
                     </div>
                     <Form.Control asChild>
@@ -126,10 +128,10 @@ const DoctorVisitForm = () => {
                 <Form.Field className="grid mb-10" name="usersideeffects">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="font-quicksand pl-4 text-2xl font-semibold text-[#0B1E5B]">
-                        Side-Effects
+                            Side-Effects
                         </Form.Label>
                         <Form.Message className="font-quicksand text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
-                        Please enter side-effects
+                            Please enter side-effects
                         </Form.Message>
                     </div>
                     <Form.Control asChild>
@@ -145,66 +147,68 @@ const DoctorVisitForm = () => {
                 <Form.Field className="grid mb-10" name="userrecoverystatus">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="font-quicksand pl-4 text-2xl font-semibold text-[#0B1E5B]">
-                        Do the symptoms persist?
+                            Do the symptoms persist?
                         </Form.Label>
                         <Form.Message className="font-quicksand text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
-                        Please choose Yes/No
+                            Choose Yes/No
                         </Form.Message>
                     </div>
                     <Form.Control asChild>
                         <Select.Root onValueChange={handleStatusChange} defaultValue="choose an option...">
-                        <Select.Trigger asChild aria-label="recstatus">
-                        <button 
-                        className="font-quicksand box-border w-full px-4 py-4 h-16 bg-[#f2e9e4] hover:bg-[#eadbd3] hover:bg-opacity-80 font-semibold focus:bg-[#eadbd3] inline-flex appearance-none items-center justify-center rounded-full text-2xl leading-none text-[#0B1E5B] shadow-[0_0_0_1px_rgba(255,174,174,0.6)] outline-none hover:shadow-[0_0_0_1px_rgba(255,144,144,1)] focus:shadow-[0_0_0_2px_rgba(255,144,144,1)] resize-none select-none"
-                        >
-                        <Select.Value/>
-                        <Select.Icon className="ml-auto">
-                            <ChevronDownIcon />
-                        </Select.Icon>
-                        </button>
-                        </Select.Trigger>
-                        <Select.Content>
-                            <Select.ScrollUpButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
-                            <ChevronUpIcon />
-                            </Select.ScrollUpButton>
-                            <Select.Viewport className="w-full bg-[#f2e9e4] rounded-3xl shadow-[0_0_0_2px_rgba(255,144,144,1)]">
-                            <Select.Group>
-                                {["Choose an option...", "Yes", "No"].map(
-                                (f, i) => (
-                                    <Select.Item
-                                    disabled={f === "Choose an option..."}
-                                    key={`${f}-${i}`}
-                                    value={f.toLowerCase()}
-                                    className=
-                                        "font-quicksand relative flex items-center px-4 py-4 h-16 rounded-full text-2xl text-[#0B1E5B] font-semibold focus:bg-[#eadbd3] focus:outline-none cursor-pointer select-none"
-                                    >
-                                    <Select.ItemText>{f}</Select.ItemText>
-                                    <Select.ItemIndicator className="ml-auto inline-flex items-center">
-                                        <CheckIcon />
-                                    </Select.ItemIndicator>
-                                    </Select.Item>
-                                )
-                                )}
-                            </Select.Group>
-                            </Select.Viewport>
-                            <Select.ScrollDownButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
-                            <ChevronDownIcon />
-                            </Select.ScrollDownButton>
-                        </Select.Content>
+                            <Select.Trigger asChild aria-label="choose recovery status">
+                                <button 
+                                className="font-quicksand box-border w-full px-4 py-4 h-16 bg-[#f2e9e4] hover:bg-[#eadbd3] hover:bg-opacity-80 font-semibold focus:bg-[#eadbd3] inline-flex appearance-none items-center justify-center rounded-full text-2xl leading-none text-[#0B1E5B] shadow-[0_0_0_1px_rgba(255,174,174,0.6)] outline-none hover:shadow-[0_0_0_1px_rgba(255,144,144,1)] focus:shadow-[0_0_0_2px_rgba(255,144,144,1)] resize-none select-none"
+                                >
+                                    <Select.Value/>
+                                    <Select.Icon className="ml-auto">
+                                        <ChevronDownIcon />
+                                    </Select.Icon>
+                                </button>
+                            </Select.Trigger>
+                            <Select.Content>
+                                <Select.ScrollUpButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
+                                    <ChevronUpIcon />
+                                </Select.ScrollUpButton>
+                                <Select.Viewport className="w-full bg-[#f2e9e4] rounded-3xl shadow-[0_0_0_2px_rgba(255,144,144,1)]">
+                                    <Select.Group>
+                                        {["Choose an option...", "Yes", "No"].map(
+                                            (f, i) => (
+                                                <Select.Item
+                                                disabled={f === "Choose an option..."}
+                                                key={`${f}-${i}`}
+                                                value={f.toLowerCase()}
+                                                className=
+                                                "font-quicksand relative flex items-center px-4 py-4 h-16 rounded-full text-2xl text-[#0B1E5B] font-semibold focus:bg-[#eadbd3] focus:outline-none cursor-pointer select-none"
+                                                >
+                                                    <Select.ItemText>{f}</Select.ItemText>
+                                                    <Select.ItemIndicator className="ml-auto inline-flex items-center">
+                                                        <CheckIcon />
+                                                    </Select.ItemIndicator>
+                                                </Select.Item>
+                                            )
+                                        )}
+                                    </Select.Group>
+                                </Select.Viewport>
+                                <Select.ScrollDownButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
+                                    <FontAwesomeIcon icon={faChevronDown} size="sm" style={{color: '#0B1E5B'}}/>
+                                </Select.ScrollDownButton>
+                            </Select.Content>
                         </Select.Root>
                     </Form.Control>
                 </Form.Field>
                 <Form.Field className="grid mb-10" name="prescriptionfile">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="font-quicksand pl-4 text-2xl font-semibold text-[#0B1E5B]">
-                        Add Prescription
+                            Add Prescription
                         </Form.Label>
                         <Form.Message className="font-quicksand text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
-                        Please upload the prescription
+                            Please upload file
                         </Form.Message>
                     </div>
+                    <Form.Control asChild>
+                        <input type="file" id="fileupload" onChange={handleChange} hidden required/>
+                    </Form.Control>
                     <div className='flex items-center'>
-                        <input type="file" id="fileupload" onChange={handleChange} hidden required/><br />
                         <label 
                         htmlFor="fileupload" 
                         className='font-quicksand cursor-pointer box-border w-56 px-4 py-4 bg-[#f2e9e4] hover:bg-[#eadbd3] hover:bg-opacity-80 focus:bg-[#eadbd3] font-semibold inline-flex appearance-none rounded-full text-2xl justify-center items-center leading-none text-[#0B1E5B] shadow-[0_0_0_1px_rgba(255,174,174,1)] outline-none hover:shadow-[0_0_0_1px_rgba(255,144,144,1)] focus:shadow-[0_0_0_2px_rgba(255,144,144,1)] selection:text-[#ffffff] selection:bg-[#ffaeae] selection:bg-opacity-60 resize-none placeholder:text-blackA6 caret-blackA6'
@@ -215,7 +219,7 @@ const DoctorVisitForm = () => {
                     </div>
                 </Form.Field>
                 <Form.Submit asChild>
-                    <button className="box-border w-full text-[#0B1E5B] hover:text-[#9aaff3] inline-flex h-16 items-center justify-center rounded-full bg-[#f6a290] hover:bg-[#f6d1cc] px-4 py-4 text-2xl font-semibold leading-none focus:outline-none mt-5 mb-5 transition-colors duration-200">
+                    <button className="box-border w-full text-[#0B1E5B] hover:text-[#9aaff3] inline-flex h-16 items-center justify-center rounded-full bg-[#f6a290] hover:bg-[#f6d1cc] px-4 py-4 text-2xl font-semibold leading-none focus:outline-none mt-3 mb-5 transition-colors duration-200">
                         Submit
                     </button>
                 </Form.Submit>
--- ehr-soulbound/src/app/lab-report/page.tsx
+++ ehr-soulbound/src/app/lab-report/page.tsx
@@ -5,6 +5,8 @@ import Image from 'next/image';
 import * as Form from '@radix-ui/react-form'
 import * as Select from '@radix-ui/react-select'
 import { CheckIcon, ChevronUpIcon, ChevronDownIcon } from '@radix-ui/react-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 export default function page() {
 
@@ -59,66 +61,68 @@ export default function page() {
                 <Form.Field className="grid mb-10" name="labtest">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="font-quicksand pl-4 text-2xl font-semibold text-[#0B1E5B]">
-                        Lab Test
+                            Lab Test
                         </Form.Label>
-                        <Form.Message className="font-quicksand text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
-                        Please choose the lab test
+                        <Form.Message className="font-quicksand ml-auto text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
+                            Please choose lab test
                         </Form.Message>
                     </div>
                     <Form.Control asChild>
                         <Select.Root onValueChange={handleStatusChange} defaultValue="select the lab test...">
-                        <Select.Trigger asChild aria-label="recstatus">
-                        <button 
-                        className="font-quicksand box-border w-full px-4 py-4 h-16 bg-[#f2e9e4] hover:bg-[#eadbd3] hover:bg-opacity-80 font-semibold focus:bg-[#eadbd3] inline-flex appearance-none items-center justify-center rounded-full text-2xl leading-none text-[#0B1E5B] shadow-[0_0_0_1px_rgba(255,174,174,0.6)] outline-none hover:shadow-[0_0_0_1px_rgba(255,144,144,1)] focus:shadow-[0_0_0_2px_rgba(255,144,144,1)] resize-none select-none"
-                        >
-                        <Select.Value/>
-                        <Select.Icon className="ml-auto">
-                            <ChevronDownIcon />
-                        </Select.Icon>
-                        </button>
-                        </Select.Trigger>
-                        <Select.Content>
-                            <Select.ScrollUpButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
-                                <ChevronUpIcon className='text-black'/>
-                            </Select.ScrollUpButton>
-                            <Select.Viewport className="w-full bg-[#f2e9e4] rounded-3xl shadow-[0_0_0_2px_rgba(255,144,144,1)]">
-                            <Select.Group>
-                                {["Select the lab test...", "Complete Blood Count", "FSH", "Glucose", "Lipid Profile", "Liver Function Test", "Renal Function Test", "Serum Electrolytes", "Thyroid Profile"].map(
-                                (f, i) => (
-                                    <Select.Item
-                                    disabled={f === "Choose an option..."}
-                                    key={`${f}-${i}`}
-                                    value={f.toLowerCase()}
-                                    className=
-                                        "font-quicksand relative flex items-center px-4 py-4 h-16 rounded-full text-2xl text-[#0B1E5B] font-semibold focus:bg-[#eadbd3] focus:outline-none cursor-pointer select-none"
-                                    >
-                                    <Select.ItemText>{f}</Select.ItemText>
-                                    <Select.ItemIndicator className="ml-auto inline-flex items-center">
-                                        <CheckIcon />
-                                    </Select.ItemIndicator>
-                                    </Select.Item>
-                                )
-                                )}
-                            </Select.Group>
-                            </Select.Viewport>
-                            <Select.ScrollDownButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
-                                <ChevronDownIcon className='text-black'/>
-                            </Select.ScrollDownButton>
-                        </Select.Content>
+                            <Select.Trigger asChild aria-label="choose lab test">
+                                <button 
+                                className="font-quicksand box-border w-full px-4 py-4 h-16 bg-[#f2e9e4] hover:bg-[#eadbd3] hover:bg-opacity-80 font-semibold focus:bg-[#eadbd3] inline-flex appearance-none items-center justify-center rounded-full text-2xl leading-none text-[#0B1E5B] shadow-[0_0_0_1px_rgba(255,174,174,0.6)] outline-none hover:shadow-[0_0_0_1px_rgba(255,144,144,1)] focus:shadow-[0_0_0_2px_rgba(255,144,144,1)] resize-none select-none"
+                                >
+                                <Select.Value />
+                                <Select.Icon className="ml-auto">
+                                    <ChevronDownIcon />
+                                </Select.Icon>
+                                </button>
+                            </Select.Trigger>
+                            <Select.Content>
+                                <Select.ScrollUpButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
+                                    <ChevronUpIcon className='text-black'/>
+                                </Select.ScrollUpButton>
+                                <Select.Viewport className="w-full bg-[#f2e9e4] rounded-3xl shadow-[0_0_0_2px_rgba(255,144,144,1)]">
+                                    <Select.Group>
+                                        {["Select the lab test...", "Complete Blood Count", "FSH", "Glucose", "Lipid Profile", "Liver Function Test", "Renal Function Test", "Serum Electrolytes", "Thyroid Profile"].map(
+                                        (f, i) => (
+                                            <Select.Item
+                                            disabled={f === "Select the lab test..."}
+                                            key={`${f}-${i}`}
+                                            value={f.toLowerCase()}
+                                            className=
+                                                "font-quicksand relative flex items-center px-4 py-4 h-16 rounded-full text-2xl text-[#0B1E5B] font-semibold focus:bg-[#eadbd3] focus:outline-none cursor-pointer select-none"
+                                            >
+                                            <Select.ItemText>{f}</Select.ItemText>
+                                            <Select.ItemIndicator className="ml-auto inline-flex items-center">
+                                                <CheckIcon />
+                                            </Select.ItemIndicator>
+                                            </Select.Item>
+                                        )
+                                        )}
+                                    </Select.Group>
+                                </Select.Viewport>
+                                <Select.ScrollDownButton className="flex items-center justify-center text-gray-700 dark:text-gray-300">
+                                    <FontAwesomeIcon icon={faChevronDown} size="sm" style={{color: '#0B1E5B'}}/>
+                                </Select.ScrollDownButton>
+                            </Select.Content>
                         </Select.Root>
                     </Form.Control>
                 </Form.Field>
                 <Form.Field className="grid mb-10" name="labreportfile">
                     <div className="flex items-baseline justify-between">
                         <Form.Label className="font-quicksand pl-4 text-2xl font-semibold text-[#0B1E5B]">
-                        Add Lab Reports
+                            Add Lab Reports
                         </Form.Label>
-                        <Form.Message className="font-quicksand text-xl text-[#0B1E5B] opacity-[0.8]" match="valueMissing">
-                        Please upload lab reports
+                        <Form.Message className="font-quicksand ml-auto text-xl text-[#0B1E5B] opacity-[0.8]" match='valueMissing'>
+                            Please upload file
                         </Form.Message>
                     </div>
+                    <Form.Control asChild>
+                        <input type="file" id="fileupload" onChange={handleChange} hidden required/>
+                    </Form.Control>
                     <div className='flex items-center'>
-                        <input type="file" id="fileupload" onChange={handleChange} hidden required/><br />
                         <label 
                         htmlFor="fileupload" 
                         className='font-quicksand cursor-pointer box-border w-56 px-4 py-4 bg-[#f2e9e4] hover:bg-[#eadbd3] hover:bg-opacity-80 focus:bg-[#eadbd3] font-semibold inline-flex appearance-none rounded-full text-2xl justify-center items-center leading-none text-[#0B1E5B] shadow-[0_0_0_1px_rgba(255,174,174,1)] outline-none hover:shadow-[0_0_0_1px_rgba(255,144,144,1)] focus:shadow-[0_0_0_2px_rgba(255,144,144,1)] selection:text-[#ffffff] selection:bg-[#ffaeae] selection:bg-opacity-60 resize-none placeholder:text-blackA6 caret-blackA6'
@@ -129,7 +133,7 @@ export default function page() {
                     </div>
                 </Form.Field>
                 <Form.Submit asChild>
-                    <button className="box-border w-full text-[#0B1E5B] hover:text-[#9aaff3] inline-flex h-16 items-center justify-center rounded-full bg-[#f6a290] hover:bg-[#f6d1cc] px-4 py-4 text-2xl font-semibold leading-none focus:outline-none mt-5 mb-5 transition-colors duration-200">
+                    <button className="box-border w-full text-[#0B1E5B] hover:text-[#9aaff3] inline-flex h-16 items-center justify-center rounded-full bg-[#f6a290] hover:bg-[#f6d1cc] px-4 py-4 text-2xl font-semibold leading-none focus:outline-none mt-3 mb-5 transition-colors duration-200">
                         Submit
                     </button>
                 </Form.Submit>
