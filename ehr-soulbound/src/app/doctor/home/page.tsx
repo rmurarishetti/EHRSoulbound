e3ade4ac1f1f8a2c786040e0b7086bb06921b176
Sashank D <harsha.desu@hotmail.com>
2023-10-22 17:52:36 +0530

    wip: doctorpage

--- ehr-soulbound/src/app/doctor/home/page.tsx
+++ ehr-soulbound/src/app/doctor/home/page.tsx
@@ -7,7 +7,7 @@ export default function page() {
             <h3 className='font-quicksand text-5xl px-5 py-5 mb-auto font-medium text-[#0B1E5B]'>Welcome, Dr.XYZ</h3>
         </div>
         <div className='flex basis-full justify-center items-center'>
-          <div className="w-3/4 flex h-[60px] justify-between text-[#635dff] font-bold px-10 pt-5 rounded-xl" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+          <div className="w-3/4 flex h-[60px] justify-between text-[#635dff] font-bold px-10 pt-5 mb-auto rounded-xl" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
             <div>Patient Name</div>
             <div>Disease</div>
             <div>Symptoms</div>
--- ehr-soulbound/src/app/user/home/page.tsx
+++ ehr-soulbound/src/app/user/home/page.tsx
@@ -4,13 +4,18 @@ import Link from "next/link";
 export default withPageAuthRequired(async function UserPage() {
   
   return (
-    <div className="flex">
-        <div className="pt-10 pr-5 ml-auto">
-            <Link className="border-[2px] border-[#ffaeae] rounded-full bg-[#f2e9e4] hover:bg-[#eadbd3] hover:border-[#ff9090] py-4 px-4 font-sans text-base font-medium text-[#0B1EB5]" href="/doctor-visit">Add Doctor&apos;s Visit</Link>
+    <div className="flex justify-center items-center">
+      <div className="flex justify-between">
+        <div className="p-10">
+            <Link className="border-[2px] border-[#ffaeae] rounded-full bg-[#f2e9e4] hover:bg-[#eadbd3] hover:border-[#ff9090] py-4 px-4 font-sans text-base font-medium text-[#0B1EB5]" href="./doctor-visit">Add Doctor&apos;s Visit</Link>
         </div>
-        <div className="pt-10 pl-5 mr-auto">
-            <Link className="border-[2px] border-[#ffaeae] rounded-full bg-[#f2e9e4] hover:bg-[#eadbd3] hover:border-[#ff9090] py-4 px-4 font-sans text-base font-medium text-[#0B1EB5]" href="/lab-report">Add Lab Reports</Link>
+        <div className="p-10">
+            <Link className="border-[2px] border-[#ffaeae] rounded-full bg-[#f2e9e4] hover:bg-[#eadbd3] hover:border-[#ff9090] py-4 px-4 font-sans text-base font-medium text-[#0B1EB5]" href="./lab-report">Add Lab Reports</Link>
         </div>
+        <div className="p-10">
+            <Link className="border-[2px] border-[#ffaeae] rounded-full bg-[#f2e9e4] hover:bg-[#eadbd3] hover:border-[#ff9090] py-4 px-4 font-sans text-base font-medium text-[#0B1EB5]" href="./patient-records">View Records</Link>
+        </div>
+      </div>
     </div>
   )
 }, { returnTo: '/user-page' });
