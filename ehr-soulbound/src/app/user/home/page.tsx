ad3547d313b594beb665a44d54e5b47fedcbfecd
Sashank D <harsha.desu@hotmail.com>
2023-12-29 12:20:09 +0530

    wip: user-home

--- ehr-soulbound/src/app/user/home/page.tsx
+++ ehr-soulbound/src/app/user/home/page.tsx
@@ -9,14 +9,14 @@ export default withPageAuthRequired(async function UserPage() {
 
   return (
     <div>
-      <div className="ml-16 font-quicksand font-medium text-4xl px-5 pt-5 pb-2 text-[#0B1E5B]">
+      <div className="ml-16 font-quicksand font-medium text-4xl px-5 pt-10 pb-2 text-[#0B1E5B]">
         Hello, {user.name}
       </div>
       <div className="ml-16 font-quicksand font-medium text-2xl px-5 pt-2 text-[#0B1E5B]">
         Welcome To Your Dashboard
       </div>
       <div className="flex justify-center items-center">
-        <div className="flex justify-between mt-24 mb-16">
+        <div className="flex justify-between mt-20 mb-16">
           <Link className="border-[2px] border-[#ffaeae] rounded-3xl bg-[#f2e9e4] hover:bg-[#eadbd3] hover:border-[#ff9090] py-4 px-4 m-10 font-quicksand font-medium text-[#0b1e5b] transition ease-in-out delay-50 duration-200" href="./doctor-visit">
             <div className="text-2xl mb-10 inline-flex items-center">
               <h3>
