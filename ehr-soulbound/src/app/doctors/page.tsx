4f1f6dd3352aac6d255226b12d3b42a779e0dcc3
Sashank D <harsha.desu@hotmail.com>
2024-01-26 18:16:26 +0530

    wip mobile-comp: meet-doctors-pg

--- ehr-soulbound/src/app/doctors/page.tsx
+++ ehr-soulbound/src/app/doctors/page.tsx
@@ -1,7 +1,7 @@
 const page = () => {
   return (
     <div>
-      <div className="flex w-full font-quicksand font-medium text-4xl p-5 items-center justify-center text-[#0B1E5B]">
+      <div className="flex w-full font-quicksand font-medium lg:text-4xl md:text-2xl text-lg p-5 items-center justify-center text-[#0B1E5B]">
         Meet Our Doctors
       </div>
     </div>
